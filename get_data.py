31af778a1e50cf759a2c6218483666d61b00cdc7
nbest <best.nicholasj@gmail.com>
2017-01-26 21:48:43 -0500

    Add files via upload

--- get_data.py
+++ get_data.py
@@ -3,14 +3,14 @@ import datetime
 
 quandl.ApiConfig.api_key = 'o8m4zQE4JiyjM4fhU7G4'
 
-start = datetime.date(2013,01,01)
-end = datetime.date(2017,01,23)
+start = datetime.date(2013,1,1)
+end = datetime.date(2017,1,23)
 
 SPY = quandl.get("YAHOO/INDEX_GSPC", start_date = start, end_date = end)
 SPY_pct_change = SPY.pct_change()
 
-print SPY_pct_change
+print(SPY_pct_change)
 
 # set file path and name
 
-SPY_pct_change.to_csv("C:\\Users\\nbest\\Documents\\_Python\\data\\"+'SPY.csv')
+SPY_pct_change.to_csv('SPY.csv')
--- update_data.py
+++ update_data.py
@@ -1,17 +1,33 @@
 import csv
+from datetime import datetime, timedelta, date
 
 ### Retrieves the latest date in the file (the bottom of the file)
 
 import csv
-with open('example.csv', 'rb') as f:
+with open('SPY.csv', 'rb') as f:
 	row_count = sum(1 for row in f)
 	f.close()
 
-with open('example.csv','rb') as g:
+with open('SPY.csv','rb') as g:
 	mycsv = csv.reader(g)
 	mycsv = list(mycsv)
 	text = mycsv[row_count-1][0]
-print text
+print(text)
+
+### Updates to the next missing day
+
+date = datetime.strptime(text, "%Y-%m-%d")
+modified_date = date + timedelta(days=1)
+datetime.strftime(modified_date, "%Y-%m-%d")
+print(modified_date)
+print("BREAK")
+
+### Downloads Quandl data from next missing day to today
+
+if date.today() == modified_date:
+	print("same")
+else:
+	print("different")
 
 #with open('example.csv', 'ab') as file:
 #	newdata = ['4','4','4']
